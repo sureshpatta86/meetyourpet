11be05ec16c8d94f6703921a4a559a8f8430fe60
suresh <sureshnani86@gmail.com>
2023-07-16 16:22:30 +0530

    Folder Structure Created

---
+++ src/app/(petshopapp)/account/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const AccountPage = () => {
+  return (
+    <div>Account Page</div>
+  )
+}
+
+export default AccountPage
---
+++ src/app/(petshopapp)/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+const SignUpPage = () => {
+  return (
+    <div>SignUp Page</div>
+  )
+}
+
+export default SignUpPage
